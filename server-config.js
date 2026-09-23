1aed260829f4b3643cad41efffe249aeff32903c
swwa71 <88960440+swwa71@users.noreply.github.com>
2026-09-23 23:04:00 +0300

    Update server-config.js

--- server-config.js
+++ server-config.js
@@ -1,8 +1,12 @@
-// إعدادات الربط بالسيرفر الخاص بـ Cloudflare
 window.SERVER_URL = "https://student-sampling-base-attribute.trycloudflare.com";
 window.API_URL = "https://student-sampling-base-attribute.trycloudflare.com";
 
 window.SERVER_CONFIG = {
+  serverUrl: "https://student-sampling-base-attribute.trycloudflare.com",
+  apiUrl: "https://student-sampling-base-attribute.trycloudflare.com"
+};
+
+window.PORTAL_CONFIG = {
   serverUrl: "https://student-sampling-base-attribute.trycloudflare.com",
   apiUrl: "https://student-sampling-base-attribute.trycloudflare.com"
 };
