cf6844b43658723fc3aa98f20484d7b6e6f73f24
swwa71 <88960440+swwa71@users.noreply.github.com>
2026-09-24 00:52:55 +0300

    Update server-config.js

--- server-config.js
+++ server-config.js
@@ -1,3 +1,19 @@
+const SERVER_BASE_URL = "https://pixels-pipes-lines-leaving.trycloudflare.com";
+
 window.BushrakomServer = {
-  baseUrl: "https://pixels-pipes-lines-leaving.trycloudflare.com"
+  baseUrl: SERVER_BASE_URL
+};
+
+// للتوافق مع أي ملف قديم يستخدم هذه الأسماء
+window.SERVER_URL = SERVER_BASE_URL;
+window.API_URL = SERVER_BASE_URL;
+
+window.SERVER_CONFIG = {
+  serverUrl: SERVER_BASE_URL,
+  apiUrl: SERVER_BASE_URL
+};
+
+window.PORTAL_CONFIG = {
+  serverUrl: SERVER_BASE_URL,
+  apiUrl: SERVER_BASE_URL
 };
